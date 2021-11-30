37741c1af7c95a1ae694279f2911162021eb3f88
Shorockhov-A <andr2409@bk.ru>
2021-11-30 10:21:15 +0300

    added db_index to is_active field of Product model

--- products/models.py
+++ products/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
-    is_active = models.BooleanField(verbose_name='активна', default=True)
+    is_active = models.BooleanField(db_index=True, verbose_name='активна', default=True)
 
     def __str__(self):
         return f"{self.name} | {self.category.name}"
