d8f5dd838194389714e897096dbaf5c97bdfd4b7
Shorockhov-A <andr2409@bk.ru>
2021-12-01 16:13:30 +0300

    added get_items_cached method to Basket model

--- baskets/models.py
+++ baskets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.functional import cached_property
 
 from users.models import User
 from products.models import Product
@@ -14,7 +15,7 @@ class BasketQuerySet(models.QuerySet):
 
 
 class Basket(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
@@ -23,20 +24,29 @@ class Basket(models.Model):
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
 
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
     @staticmethod
     def get_item(pk):
         return Basket.objects.filter(pk=pk).select_related().first()
 
+    @property
     def sum(self):
         return self.product.price * self.quantity
 
+    @property
     def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user).select_related()
-        return sum(basket.quantity for basket in baskets)
+        _items = self.get_items_cached
+        _total_quantity = sum(list(map(lambda x: x.quantity, _items)))
+        return _total_quantity
 
+    @property
     def total_sum(self):
-        baskets = Basket.objects.filter(user=self.user).select_related()
-        return sum(basket.sum() for basket in baskets)
+        _items = self.get_items_cached
+        _total_sum = sum(list(map(lambda x: x.sum, _items)))
+        return _total_sum
 
     def save(self, *args, **kwargs):
         if self.pk:
