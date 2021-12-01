f248af64e5633c5696fa1a74d99cdac8dd9403c2
Shorockhov-A <andr2409@bk.ru>
2021-12-01 18:41:46 +0300

    added Memcached caching

--- products/views.py
+++ products/views.py
@@ -4,7 +4,11 @@ from django.urls import reverse
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 
+from django.conf import settings
+from django.core.cache import cache
+
 from products.models import ProductCategory, Product
+
 
 # Create your views here.
 
@@ -32,15 +36,64 @@ class ProductsListView(ListView):
         context.update({
             'title': 'GeekShop - Каталог',
             'header': 'GeekShop',
-            'categories': ProductCategory.objects.all().select_related(),
+            'categories': get_links_menu(),
         })
         return context
 
     def get_queryset(self):
         if self.kwargs.get('category'):
-            category = get_object_or_404(ProductCategory, id=self.kwargs['category'])
-            return Product.objects.filter(category=category).select_related()
-        return Product.objects.all().select_related()
+            category = get_category(self.kwargs['category'])
+            return get_products_in_category_ordered_by_price(category.pk)
+        return get_products()
+
+
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
+
+def get_category(pk):
+    if settings.LOW_CACHE:
+        key = f'category_{pk}'
+        category = cache.get(key)
+        if category is None:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            cache.set(key, category)
+        return category
+    else:
+        return get_object_or_404(ProductCategory, pk=pk)
+
+
+def get_products():
+    if settings.LOW_CACHE:
+        key = 'products'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+
+
+def get_products_in_category_ordered_by_price(pk):
+    if settings.LOW_CACHE:
+        key = f'products_in_category_ordered_by_price_{pk}'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(category__pk=pk, is_active=True,
+                                              category__is_active=True).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
 
 
 # def index(request):
