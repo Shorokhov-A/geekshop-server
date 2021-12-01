427999ab33d689d8ed9090b5805fdfacce6c0101
Shorockhov-A <andr2409@bk.ru>
2021-12-01 17:09:04 +0300

    added "with" tag

--- orders/models.py
+++ orders/models.py
@@ -34,17 +34,17 @@ class Order(models.Model):
     def __str__(self):
         return f'Текущий заказ: {self.id}'
 
-    def get_total_quantity(self):
-        items = self.orderitems.select_related()
-        return sum(list(map(lambda x: x.quantity, items)))
+    # def get_total_quantity(self):
+    #     items = self.orderitems.select_related()
+    #     return sum(list(map(lambda x: x.quantity, items)))
 
     def get_product_type_quantity(self):
         items = self.orderitems.select_related()
         return len(items)
 
-    def get_total_cost(self):
-        items = self.orderitems.select_related()
-        return sum(list(map(lambda x: x.quantity * x.product.price, items)))
+    # def get_total_cost(self):
+    #     items = self.orderitems.select_related()
+    #     return sum(list(map(lambda x: x.quantity * x.product.price, items)))
 
     def delete(self, using=None, keep_parents=False):
         for item in self.orderitems.select_related():
@@ -52,6 +52,13 @@ class Order(models.Model):
             item.product.save()
         self.is_active = False
         self.save()
+
+    def get_summary(self):
+        items = self.orderitems.select_related()
+        return {
+            'total_cost': sum(list(map(lambda x: x.quantity * x.product.price, items))),
+            'total_quantity': sum(list(map(lambda x: x.quantity, items))),
+        }
 
 
 class OrderItemQuerySet(models.QuerySet):
--- orders/views.py
+++ orders/views.py
@@ -42,8 +42,8 @@ class OrderItemsCreate(CreateView):
         else:
             basket_items = Basket.objects.filter(user=self.request.user).select_related()
             if basket_items:
-                total_quantity = basket_items[0].total_quantity()
-                total_sum = basket_items[0].total_sum()
+                total_quantity = basket_items[0].total_quantity
+                total_sum = basket_items[0].total_sum
             if len(basket_items):
                 order_formset = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=len(basket_items))
                 formset = order_formset()
