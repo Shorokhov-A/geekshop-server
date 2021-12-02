53ca3e4d7a428c98a7d219f7818b3dcc4c62cc41
Shorockhov-A <andr2409@bk.ru>
2021-12-02 15:09:36 +0300

    added AJAX and page cache

--- products/views.py
+++ products/views.py
@@ -1,11 +1,14 @@
-from django.http import JsonResponse, HttpResponseRedirect
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from django.urls import reverse
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 
 from django.conf import settings
 from django.core.cache import cache
+
+from django.template.loader import render_to_string
+from django.views.decorators.cache import cache_page
 
 from products.models import ProductCategory, Product
 
@@ -83,6 +86,18 @@ def get_products():
         return Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
 
 
+def get_products_ordered_by_price():
+    if settings.LOW_CACHE:
+        key = 'products_ordered_by_price'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+
+
 def get_products_in_category_ordered_by_price(pk):
     if settings.LOW_CACHE:
         key = f'products_in_category_ordered_by_price_{pk}'
@@ -94,6 +109,40 @@ def get_products_in_category_ordered_by_price(pk):
         return products
     else:
         return Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
+
+
+def products_ajax(request, pk=None, page=1):
+    if request.is_ajax():
+        links_menu = get_links_menu()
+        if pk:
+            if pk == '0':
+                category = {
+                    'pk': 0,
+                    'name': 'все'
+                }
+                products = get_products_ordered_by_price()
+            else:
+                category = get_category(pk)
+                products = get_products_in_category_ordered_by_price(pk)
+
+            paginator = Paginator(products, 2)
+            try:
+                products_paginator = paginator.page(page)
+            except PageNotAnInteger:
+                products_paginator = paginator.page(1)
+            except EmptyPage:
+                products_paginator = paginator.page(paginator.num_pages)
+
+            content = {
+                'links_menu': links_menu,
+                'category': category,
+                'products': products_paginator,
+            }
+            result = render_to_string(
+                'products/includes/inc_products_list_content.html',
+                context=content,
+                request=request)
+            return JsonResponse({'result': result})
 
 
 # def index(request):
