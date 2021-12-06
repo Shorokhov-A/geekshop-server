c8c7d2f2ec2d7d98e8a17305feada237d3a0b5c9
Shorockhov-A <andr2409@bk.ru>
2021-12-07 02:16:51 +0300

    added learn_db.py

---
+++ products/management/commands/learn_db.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from products.models import ProductCategory, Product
+from django.db import connection
+from django.db.models import Q
+from products.views import db_profile_by_type
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        test_products = Product.objects.filter(
+            Q(category__name='Одежда') |
+            Q(category__name='Аксессуары')
+        )
+        print(len(test_products))
+        # print(test_products)
+        db_profile_by_type('learn db', '', connection.queries)
