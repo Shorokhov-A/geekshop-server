66fcf59152b8473a91cfa7aa601321eed581fe14
Shorockhov-A <andr2409@bk.ru>
2021-12-07 13:58:45 +0300

    added test_user_login and test_user_logout

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from django.test.client import Client
+from users.models import User
+from django.core.management import call_command
 
-# Create your tests here.
+from django.conf import settings
+
+
+class TestUserManagement(TestCase):
+    def setUp(self):
+        call_command('flush', '--noinput')
+        call_command('loaddata', 'test_db.json')
+        self.client = Client()
+        self.superuser = User.objects.create_superuser('django2', 'django2@geekshop.local', 'geekbrains')
+        self.user = User.objects.create_user('tarantino', 'tarantino@geekshop.local', 'geekbrains')
+        self.user_with__first_name = User.objects.create_user('umaturman', 'umaturman@geekshop.local',
+                                                              'geekbrains', first_name='Ума')
+
+    def test_user_login(self):
+        # главная без логина
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
+        self.assertEqual(response.context['title'], 'главная')
+        self.assertNotContains(response, 'Пользователь', status_code=200)
+        # self.assertNotIn('Пользователь', response.content.decode())
+        # данные пользователя
+        self.client.login(username='tarantino', password='geekbrains')
+
+        # логинимся
+        response = self.client.get('/users/login/')
+        self.assertFalse(response.context['user'].is_anonymous)
+        self.assertEqual(response.context['user'], self.user)
+
+        # главная после логина
+        response = self.client.get('/')
+        self.assertContains(response, 'Пользователь', status_code=200)
+        self.assertEqual(response.context['user'], self.user)
+        # self.assertIn('Пользователь', response.content.decode())
+
+    def test_user_logout(self):
+        # данные пользователя
+        self.client.login(username='tarantino', password='geekbrains')
+
+        # логинимся
+        response = self.client.get('/users/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['user'].is_anonymous)
+
+        # выходим из системы
+        response = self.client.get('/users/logout/')
+        self.assertEqual(response.status_code, 302)
+
+        # главная после выхода
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
+
+    def tearDown(self):
+        call_command('sqlsequencereset', 'products', 'users', 'orders',
+                     'baskets')
